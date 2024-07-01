38b30f103a67a0449dca7a4b045349204b50755a
Argel Michinel <66443739+ArgelMichinel@users.noreply.github.com>
2024-06-30 22:38:01 -0300

    Update DatabaseConnection.php
    Eliminación de parametro de conexión

--- include2/DatabaseConnection.php
+++ include2/DatabaseConnection.php
@@ -1,5 +1,4 @@
 <?php
 
-$pdo = new PDO('mysql:host=database-1.cnxbwilgrgwt.us-east-1.rds.amazonaws.com;dbname=id21371385_mercadofree2;charset=utf8', 'root', 'odfknoiesdfkl42');
 
 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
